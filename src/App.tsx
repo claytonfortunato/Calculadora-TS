594ff03b7cf38401ca04377cd88e326bb2c94268
claytonfortunato <claytonreis10@gmail.com>
2023-03-28 23:39:56 -0300

    feat: button add

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import * as C from "./styles";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <C.Container>
+      <h1>Calculadora</h1>
+      <C.Calculate>
+        <C.Buttons>
+          <button>AC</button>
+          <button>C</button>
+          <button>%</button>
+          <button>/</button>
+
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+
+          <button>0</button>
+          <button>.</button>
+        </C.Buttons>
+      </C.Calculate>
+    </C.Container>
+  );
 }
 
 export default App;
---
+++ src/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Calculate = styled.div``;
+
+export const Buttons = styled.div``;
