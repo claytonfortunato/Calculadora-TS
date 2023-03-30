c3d8f884f2e6660f3f56dd45440a6ed39fef30f1
claytonfortunato <claytonreis10@gmail.com>
2023-03-30 00:21:19 -0300

    feat: projeto concluido

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,37 @@ function App() {
     }
   };
 
+  const handleResult = () => {
+    setResult(eval(number).toString());
+  };
+
+  const handleClear = () => {
+    setNumber("0");
+    setResult("0");
+  };
+
+  const handleDeleteNumber = () => {
+    setNumber(number.substring(0, number.length - 1));
+  };
+
+  const handlePercent = () => {
+    const numberPercent = parseFloat(number);
+    setResult((numberPercent / 100).toString());
+    setNumber("0");
+  };
+
+  const handleChangeSign = () => {
+    const num = parseFloat(number);
+
+    if (number > "0") {
+      const numberNegative = num * -1;
+      setNumber(numberNegative.toString());
+    } else {
+      const numberPositive = Math.abs(num);
+      setNumber(numberPositive.toString());
+    }
+  };
+
   return (
     <C.Container>
       <C.Calculate>
@@ -43,42 +74,56 @@ function App() {
           </div>
         </C.Result>
         <C.Buttons>
-          <button className="clean">AC</button>
-          <button>C</button>
-          <button className="button_operations">
+          <button className="clean" onClick={handleClear}>
+            CE
+          </button>
+          <button onClick={handleDeleteNumber}>C</button>
+          <button onClick={handlePercent}>
             <img src={Percent} alt="" />
           </button>
 
-          <button className="button_operations">
+          <button
+            className="button_operations"
+            onClick={() => handleOperations("/")}
+          >
             <img src={Divide} alt="" />
           </button>
           <button onClick={() => handleNumber("7")}>7</button>
           <button onClick={() => handleNumber("8")}>8</button>
           <button onClick={() => handleNumber("9")}>9</button>
 
-          <button className="button_operations">
+          <button
+            className="button_operations"
+            onClick={() => handleOperations("*")}
+          >
             <img src={X} alt="" />
           </button>
           <button onClick={() => handleNumber("4")}>4</button>
           <button onClick={() => handleNumber("5")}>5</button>
           <button onClick={() => handleNumber("6")}>6</button>
 
-          <button className="button_operations">
+          <button
+            className="button_operations"
+            onClick={() => handleOperations("-")}
+          >
             <img src={Minus} alt="" />
           </button>
           <button onClick={() => handleNumber("1")}>1</button>
           <button onClick={() => handleNumber("2")}>2</button>
           <button onClick={() => handleNumber("3")}>3</button>
 
-          <button className="button_operations">
+          <button
+            className="button_operations"
+            onClick={() => handleOperations("+")}
+          >
             <img src={Plus} alt="" />
           </button>
-          <button className="button_operations">
+          <button onClick={handleChangeSign}>
             <img src={PlusMinus} alt="" />
           </button>
           <button onClick={() => handleNumber("0")}>0</button>
-          <button onClick={() => handleNumber(".")}>.</button>
-          <button className="button_equals">
+          <button onClick={() => handleNumber(".")}>,</button>
+          <button className="button_equals" onClick={handleResult}>
             <img src={BtnEqual} alt="" />
           </button>
         </C.Buttons>
--- src/styles.ts
+++ src/styles.ts
@@ -2,8 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 356px;
-  height: 560px;
-
   padding: 54px 32px 32px 32px;
 
   background: #2d2a37;
