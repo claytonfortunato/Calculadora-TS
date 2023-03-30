1b77ed8832bb0868f4de269ef72b182e6f44823d
claytonfortunato <claytonreis10@gmail.com>
2023-03-30 00:01:48 -0300

    feat: handle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import BtnEqual from "./assets/ButtonEquals.png";
 import PlusMinus from "./assets/PlusMinus.png";
 import Plus from "./assets/Plus.png";
@@ -10,20 +12,38 @@ import Equals from "./assets/Equals.png";
 import * as C from "./styles";
 
 function App() {
+  const [number, setNumber] = useState("0");
+  const [result, setResult] = useState("0");
+
+  const handleNumber = (num: any) => {
+    if (number.includes(",") && num === ",") return;
+    if (number === "0") {
+      setNumber(num);
+    } else {
+      setNumber(`${number}${num}`);
+    }
+  };
+
+  const handleOperations = (operation: string) => {
+    if (number.slice(-1) !== ".") {
+      setNumber(`${number}${operation}`);
+    }
+  };
+
   return (
     <C.Container>
       <C.Calculate>
         <C.Result>
-          <span></span>
+          <span>{number}</span>
           <div className="calculate_result">
             <span>
               <img src={Equals} alt="" />
             </span>
-            <span className="result"></span>
+            <span className="result">{result}</span>
           </div>
         </C.Result>
         <C.Buttons>
-          <button>AC</button>
+          <button className="clean">AC</button>
           <button>C</button>
           <button className="button_operations">
             <img src={Percent} alt="" />
@@ -32,23 +52,23 @@ function App() {
           <button className="button_operations">
             <img src={Divide} alt="" />
           </button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
+          <button onClick={() => handleNumber("7")}>7</button>
+          <button onClick={() => handleNumber("8")}>8</button>
+          <button onClick={() => handleNumber("9")}>9</button>
 
           <button className="button_operations">
             <img src={X} alt="" />
           </button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
+          <button onClick={() => handleNumber("4")}>4</button>
+          <button onClick={() => handleNumber("5")}>5</button>
+          <button onClick={() => handleNumber("6")}>6</button>
 
           <button className="button_operations">
             <img src={Minus} alt="" />
           </button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
+          <button onClick={() => handleNumber("1")}>1</button>
+          <button onClick={() => handleNumber("2")}>2</button>
+          <button onClick={() => handleNumber("3")}>3</button>
 
           <button className="button_operations">
             <img src={Plus} alt="" />
@@ -56,8 +76,8 @@ function App() {
           <button className="button_operations">
             <img src={PlusMinus} alt="" />
           </button>
-          <button>0</button>
-          <button>.</button>
+          <button onClick={() => handleNumber("0")}>0</button>
+          <button onClick={() => handleNumber(".")}>.</button>
           <button className="button_equals">
             <img src={BtnEqual} alt="" />
           </button>
--- src/styles.ts
+++ src/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 356px;
-  height: 510px;
+  height: 560px;
 
   padding: 54px 32px 32px 32px;
 
@@ -82,10 +82,11 @@ export const Buttons = styled.div`
     align-items: center;
 
     background: linear-gradient(
-      180deg,
-      rgba(0, 0, 0, 0.05) 0%,
-      rgba(255, 255, 255, 0.05) 100%
-    );
+        180deg,
+        rgba(0, 0, 0, 0.05) 0%,
+        rgba(255, 255, 255, 0.05) 100%
+      ),
+      #2d2a37;
 
     box-shadow: 0px 11px 7px rgba(0, 0, 0, 0.01),
       0px 7px 7px rgba(0, 0, 0, 0.04), 0px 4px 6px rgba(0, 0, 0, 0.1),
@@ -93,6 +94,10 @@ export const Buttons = styled.div`
       inset 0px 2px 3px rgba(255, 255, 255, 0.06);
 
     border-radius: 999px;
+  }
+
+  .clean {
+    color: #975dfa;
   }
 
   .button_operations {
